9298054881112587669f695491d9b027977f62c9
Louis J Rabeno <lrabeno@gmail.com>
2024-01-27 23:14:46 -0500

    added express and set up initial get route

--- db/index.js
+++ db/index.js
@@ -1,6 +1,18 @@
 const pg = require('pg');
-const client = new pg.Client('postgres://localhost/testDB');
+const client = new pg.Client('postgres://localhost/testdb');
+
+const getUsers = (request, response) => {
+  client.query('SELECT * FROM _USER ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
 
 client.connect();
 
-module.exports = client;
+module.exports = {
+  client,
+  getUsers,
+};
